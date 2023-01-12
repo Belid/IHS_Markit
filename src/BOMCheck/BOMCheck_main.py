147c5c2962e24d7a8ed000841433cd58cf5d1600
Mahmoud <mahmoud.belid@siemens-healthineers.com>
2023-01-12 16:34:29 +0100

    adding comments1

--- src/BOMCheck/BOMCheck_main.py
+++ src/BOMCheck/BOMCheck_main.py
@@ -8,18 +8,19 @@ class IhsMarkit:
 
     def __init__(self, report):
         self.wb = load_workbook(report)
-        self.input_sheet = self.wb["CES"]
+        self.input_sheet = self.wb["CES"]           # hier muss man die Name von sheet eingeben
         self.max_row = self.input_sheet.max_row
         self.ihs = Selenium.ChromeIhs()
         self.part_number_list = []
-        self.ordering_text_list = [8, 100,44]
-        self.ordering_text_col = 30
-        self.manufacturer_id_col = 29
-        self.part_number_col = 3
-        self.ext_datatabase_manu_col = 39
+        self.ordering_text_list = [28, 29, 30]          # das Programm sucht nur die Zeillennummer in diesem List.
+        self.ordering_text_col = 30                 # das ist Ordering Text colummnnummer
+        self.manufacturer_id_col = 29               # das ist Manufacturer ID colummnnummer
+        self.part_number_col = 3                    # das ist Part number colummnnummer
+        self.ext_datatabase_manu_col = 39           # das ist external database manufacturer colummnnummer
 
-        for row in self.ordering_text_list:
-            print("current row is #########################################################################################  " + str(row))
+        #for row in self.ordering_text_list:
+        for row in range(5, self.max_row+1):      # wenn man bestimte range von  Zeilen suchen will, dann muss den range geändert werden, range(von , bis ).
+            print("current row is ####################################  " + str(row))
             # if self.input_sheet.cell(row=row, column=40).value != "Found":
             self.manufacturer_id = self.input_sheet.cell(row=row, column=self.manufacturer_id_col).value
             self.part_number1 = self.input_sheet.cell(row=row, column=self.part_number_col).value
@@ -30,8 +31,8 @@ class IhsMarkit:
             first_word = str(self.ext_manufacturer_id).split()
             first_word = first_word[0]
             self.ext_manufacturer_id = str(first_word)
-            print(self.ext_manufacturer_id)
             print(self.ordering_text)
+            print(self.ext_manufacturer_id)
             self.table_list = self.ihs.get_ihs_table_list(self.ordering_text)
             self.table_elements = len(self.table_list)
 
@@ -50,17 +51,17 @@ class IhsMarkit:
                             self.result = self.ihs.eu_rohs_exemptions()
                             self.input_sheet.cell(row=row, column=42).value = self.Status
                             self.input_sheet.cell(row=row, column=41).value = self.result
-                            self.ihs.download_print()
+                            #self.ihs.download_print()
                             # self.ihs.download_pdf_ihs(current_row)
-                            self.rename_pdf()
-                            # self.ihs.refresh()
+                            if self.ihs.year > 2014:
+                                self.rename_pdf()
                             break
                         else:
                             self.input_sheet.cell(row=row, column=40).value = "Hersteller nicht gefunden"
                     except:
-                        self.ihs.refresh(ordering_text=self.ordering_text)
+                        print("Fehler beim {}  {} ".format(self.ordering_text, self.ext_manufacturer_id))
+                        self.ihs.refresh()
                         time.sleep(2)
-                        print("error ocuried")
 
         self.wb.save(report)
 
@@ -68,11 +69,17 @@ class IhsMarkit:
         self.part_number_list.append(self.part_number1)
         count = self.part_number_list.count(self.part_number1)
         if count == 1:
-            rename(r"C:\Temp\test_bomcheck\download.pdf".format(self.part_number1),
-                   r"C:\Temp\test_bomcheck\{}_{}.pdf".format(self.part_number1, self.manufacturer_id))
+            try:
+                rename(r"C:\Temp\test_bomcheck\download.pdf".format(self.part_number1),
+                       r"C:\Temp\test_bomcheck\{}_{}.pdf".format(self.part_number1, self.manufacturer_id))
+            except FileExistsError:
+                print("pdf ist schon vorhanden")
         else:
-            rename(r"C:\Temp\test_bomcheck\download.pdf".format(self.part_number1),
-                   r"C:\Temp\test_bomcheck\{}_{}({}).pdf".format(self.part_number1, self.manufacturer_id, count - 1))
+            try:
+                rename(r"C:\Temp\test_bomcheck\download.pdf".format(self.part_number1),
+                       r"C:\Temp\test_bomcheck\{}_{}({}).pdf".format(self.part_number1, self.manufacturer_id, count - 1))
+            except FileExistsError:
+                print("pdf ist schon vorhanden")
 
 
 def check_input_ordering_text(ordering_text):
@@ -104,11 +111,16 @@ def change_pdf_name(part_number, manufacturer_id, part_number1, count):
                r"C:\Temp\test_bomcheck\{}_{}({}).pdf".format(part_number1, manufacturer_id, count-1))
 
 
+def test():
+    rename(r"C:\Temp\test_bomcheck\download.pdf", r"C:\Temp\test_bomcheck\10816763_VIT.pdf")
+
+
 def main(report):
 
     IhsMarkit(report)
+    # test()
 
 
 if __name__ == "__main__":
-    file = r"C:\bomcheck\BOMCheck_test1.xlsx"
+    file = r"C:\bomcheck\BOMCheck_test1.xlsx"   # Hier muss man die Dateipfad eingeben.
     main(file)
--- src/Selenium/Selenium.py
+++ src/Selenium/Selenium.py
@@ -11,13 +11,26 @@ class ChromeIhs:
         self.chrome_options = webdriver.ChromeOptions()
         self.chrome_options.add_experimental_option("detach", True)
         self.chrome_options.add_experimental_option("prefs", {"download.default_directory": "C:\\Temp\\test_bomcheck"})
+
         opt = Options()
         opt.add_experimental_option('debuggerAddress', 'localhost:9100')
         self.driver = webdriver.Chrome(executable_path="C:\\bomcheck\\chromedriver.exe", chrome_options=opt)
+        # self.driver.get("https://login.ihsmarkit.com/?v=2.0&destinationUrl=https%3A%2F%2Fsam.ihsmarkit.com%3A443%2Fsso%2Foauth2%2Fauthorize%3Fclient_id%3DEngineering-ProductDesign-ERC-Portal-Prod-4Th6JHa09r%26response_type%3Dcode%26nonce%3D73c31106-d539-4e52-bfd7-fce12230ad72%26scope%3Dopenid%2520email%2520profile%2520saml_attributes%2520account%26redirect_uri%3Dhttps%253A%252F%252Flogin.ihserc.com%252Flogin%252Ferc%253FloginCode%253DSAM_AUTHORIZATION%26state%3D%2526clientIP%253D165.225.27.0%2526subAccountId%253Dnull")
+        # time.sleep(3)
+        # self.driver.maximize_window()
+        # self.driver.find_element(By.ID, "emailAddress").send_keys("mahmoud.belid@siemens-healthineers.com")
+        # self.driver.find_element(By.ID, "continueButton").click()
+        # time.sleep(3)
+        # self.driver.find_element(By.ID, "password").send_keys("mody1modY@")
+        # self.driver.find_element(By.ID, "loginButton").click()
+        # time.sleep(2)
+        # self.driver.find_element(By.XPATH, "/html/body/div[2]/div[3]/table/tbody/tr/td/b/div/div[3]/a").click()
+        # time.sleep(12)
         self.part_number_list = []
         self.table_list = []
         self.table3_list = []
         self.table4_list = []
+        self.year = 1999
 
     def get_ihs_table_list(self, ordering_text):
         part_start_with = self.driver.find_element(By.XPATH, "/html/body/app/div[1]/bom-header/div/div/div[2]/simplesearch/div/div/div[2]/input")
@@ -25,25 +38,27 @@ class ChromeIhs:
         part_start_with.send_keys(ordering_text)
         self.driver.find_element(By.XPATH, "/html/body/app/div[1]/bom-header/div/div/div[2]/simplesearch/div/div/span/button/svg-symbol").click()
         time.sleep(3)       # first sleep
-        table = self.driver.find_element(By.XPATH, "/html/body/app/div[1]/main-content/div/div/div/part-search/div/part-search-results/div/div/cap-grid-ng/cap-grid-wrapper/ag-grid-angular/div/div[2]/div[2]/div[3]/div[1]")
-        self.table_list = list(table.text.splitlines())
-        print(self.table_list)
-        if len(self.table_list) == 0:
-            try:
-                self.driver.find_element(By.XPATH, "/html/body/app/modal-placeholder/modal-view/div/div/div/fuzzy-search-modal/modal-header/div/div/button").click()
+        body_html = self.driver.find_element(By.ID, "bomwidget")
+        body_html_text_list = list(body_html.text.splitlines())
 
-            except:
-                self.refresh(ordering_text)
+        if "No Results Found" in body_html_text_list:
+            self.driver.find_element(By.XPATH, "/html/body/app/modal-placeholder/modal-view/div/div/div/fuzzy-search-modal/modal-header/div/div/button").click()
+            self.table_list = []
+            print("ordering text: " + ordering_text + "  is not found")
+            return self.table_list
 
-        return self.table_list
+        elif "Notification" in body_html_text_list:
+            self.driver.find_element(By.XPATH, "/html/body/app/modal-placeholder/modal-view/div/div/div/low-resolution-warning/modal-header/div/div/button").click()
+            table = self.driver.find_element(By.XPATH, "/html/body/app/div[1]/main-content/div/div/div/part-search/div/part-search-results/div/div/cap-grid-ng/cap-grid-wrapper/ag-grid-angular/div/div[2]/div[2]/div[3]/div[1]")
+            self.table_list = list(table.text.splitlines())
+            print(self.table_list)
+            return self.table_list
 
-    def download_pdf_ihs(self, current_row):
-        print(current_row)
-        self.driver.find_element(By.XPATH, "/html/body/app/div[1]/main-content/div/div/div/part-search/div/part-search-results/div/div/cap-grid-ng/cap-grid-wrapper/ag-grid-angular/div/div[2]/div[2]/div[3]/div[1]/div[{}]/div[2]/div/a[2]".format(current_row)).click()
-        time.sleep(3)
-        self.driver.switch_to.window(self.driver.window_handles[1])
-        self.driver.close()
-        self.driver.switch_to.window(self.driver.window_handles[0])
+        else:
+            table = self.driver.find_element(By.XPATH, "/html/body/app/div[1]/main-content/div/div/div/part-search/div/part-search-results/div/div/cap-grid-ng/cap-grid-wrapper/ag-grid-angular/div/div[2]/div[2]/div[3]/div[1]")
+            self.table_list = list(table.text.splitlines())
+            print(self.table_list)
+            return self.table_list
 
     def get_status_value(self, current_row):
         if len(self.table_list) < 6:
@@ -68,14 +83,19 @@ class ChromeIhs:
         print(table2_list)
         print(self.table3_list)
         print(self.table4_list)
-        self.check_date()           # return the year
+        self.year = int(self.check_date())          # return the year
         status = "none"
         if table2_list[0] == "EU RoHS Version" and table2_list[1] == "RoHS 2 (2015/863/EU)":
             status = "C"
-        self.driver.find_element(By.XPATH, "/html/body/app/modal-placeholder/modal-view/div/div/div/part-details/modal-window/div[2]/div/part-details-toolbar/div/toolbar/div/div[1]/toolbar-list[2]/div/div/button/span").click()
-        self.driver.find_element(By.XPATH, "/html/body/app/modal-placeholder/modal-view/div/div/div/part-details/modal-window/div[2]/div/part-details-toolbar/div/toolbar/div/div[1]/toolbar-list[2]/div/div/div/ul/toolbar-list-item[6]").click()
-        time.sleep(1)
-        self.driver.find_element(By.XPATH, "/html/body/app/modal-placeholder/modal-view[2]/div/div/div/part-details-print-preview/div[1]/div/div/div/div/div[1]/div[3]/button[1]").click()
+        if self.year > 2014:
+            self.driver.find_element(By.XPATH, "/html/body/app/modal-placeholder/modal-view/div/div/div/part-details/modal-window/div[2]/div/part-details-toolbar/div/toolbar/div/div[1]/toolbar-list[2]/div/div/button/span").click()
+            self.driver.find_element(By.XPATH, "/html/body/app/modal-placeholder/modal-view/div/div/div/part-details/modal-window/div[2]/div/part-details-toolbar/div/toolbar/div/div[1]/toolbar-list[2]/div/div/div/ul/toolbar-list-item[6]").click()
+            time.sleep(1)
+            self.driver.find_element(By.XPATH, "/html/body/app/modal-placeholder/modal-view[2]/div/div/div/part-details-print-preview/div[1]/div/div/div/div/div[1]/div[3]/button[1]").click()
+            self.driver.find_element(By.XPATH, "/html/body/app/modal-placeholder/modal-view[2]/div/div/div/part-details-print-preview/modal-header/div/div/button").click()
+            self.driver.find_element(By.XPATH, "/html/body/app/modal-placeholder/modal-view/div/div/div/part-details/modal-window/div[1]/h3/div[2]").click()
+        else:
+            self.driver.find_element(By.XPATH, "/html/body/app/modal-placeholder/modal-view/div/div/div/part-details/modal-window/div[1]/h3/div[2]").click()
 
         return status
 
@@ -84,28 +104,42 @@ class ChromeIhs:
         if self.table4_list[0] == 'Last Verified Date':
             year = self.table4_list[1]
             year = int(year[6:])
-            print(year)
+            print("Jahr ist: " + str(year))
+
+        elif self.table3_list[0] == 'Last Verified Date':
+            year = self.table3_list[1]
+            year = int(year[6:])
+            print("Jahr ist: " + str(year))
         return year
 
     def eu_rohs_exemptions(self):
-        if self.table3_list[0] == "EU RoHS Exemptions":
+        if self.table3_list[0] == "EU RoHS Exemptions" and self.table3_list[1] == "7(c)-I":
             result = self.table3_list[1]
         else:
             result = ""
+
         return result
 
     def download_print(self):
         self.driver.find_element(By.XPATH, "/html/body/app/modal-placeholder/modal-view[2]/div/div/div/part-details-print-preview/modal-header/div/div/button").click()
         self.driver.find_element(By.XPATH, "/html/body/app/modal-placeholder/modal-view/div/div/div/part-details/modal-window/div[1]/h3/div[2]").click()
 
-    def error(self):
-        self.driver.find_element(By.XPATH, "/html/body/app/modal-placeholder/modal-view/div/div/div/fuzzy-search-modal/div[2]/button[2]").click()
-        print("x click")
-        time.sleep(3)
+    def refresh(self):
+        self.driver.get("https://4donline.ihs.com/parts-intelligence/#/search/parts")
+        time.sleep(4)
+        body_html = self.driver.find_element(By.ID, "bomwidget")
+        body_html_text_list = list(body_html.text.splitlines())
+        print(body_html_text_list)
+        if "Notification" in body_html_text_list:
+            self.driver.find_element(By.XPATH, "/html/body/app/modal-placeholder/modal-view/div/div/div/low-resolution-warning/modal-header/div/div/button").click()
 
-    def refresh(self, ordering_text):
-        self.driver.get("https://4donline.ihs.com/parts-intelligence/#/search/parts?keyword={}&type=startswith&ignore=true".format(ordering_text))
-        time.sleep(2)
+    def download_pdf_ihs(self, current_row):
+        print(current_row)
+        self.driver.find_element(By.XPATH, "/html/body/app/div[1]/main-content/div/div/div/part-search/div/part-search-results/div/div/cap-grid-ng/cap-grid-wrapper/ag-grid-angular/div/div[2]/div[2]/div[3]/div[1]/div[{}]/div[2]/div/a[2]".format(current_row)).click()
+        time.sleep(3)
+        self.driver.switch_to.window(self.driver.window_handles[1])
+        self.driver.close()
+        self.driver.switch_to.window(self.driver.window_handles[0])
 
 
 def main():
